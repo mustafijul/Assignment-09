63b536dce69cac4373ad9bd2d111333d9925f99a
mustafij <mustafijulislam21@gmail.com>
2023-12-07 10:59:31 +0600

    salary and job title added in the card

--- src/Statistics.jsx
+++ src/Statistics.jsx
@@ -26,30 +26,53 @@ const Statistics = () => {
         <h1 className="font-bold text-2xl text-center">Job details</h1>
         <img src="/src/assets/images/bg1.png" alt="" />
       </div>
-      <div>
-      <div  className="px-4 pt-5 mx-auto lg:w-1/2 md:px-24 lg:px-8">
-        {/* job responsibility */}
-        <div className="mb-4">
-        <h1> <span className="font-bold">Job Responsibility: </span> {jobs.job_responsibility}</h1>
+      {/* job details */}
+      <div className="flex">
+      {/* left side information */}
+        <div className="px-4 pt-5 mx-auto lg:w-1/2 md:px-24 lg:px-8">
+          {/* job responsibility */}
+          <div className="mb-4">
+            <h1>
+              {" "}
+              <span className="font-bold">Job Responsibility: </span>{" "}
+              {jobs.job_responsibility}
+            </h1>
+          </div>
+          {/* job description */}
+          <div className="mb-4">
+            {" "}
+            <p className="text-slate-400">
+              {" "}
+              <span className="font-bold">Job Description: </span>
+              {jobs.job_description}
+            </p>
+          </div>
+          {/* Educational requirements */}
+          <div className="mb-5">
+            <p>
+              <span className="font-bold">Educational Requirements: </span>
+            </p>
+            <p>{jobs.educational_requirements}</p>
+          </div>
+          {/* job experience */}
+          <div className="mb-5">
+            <p>
+              <span className="font-bold">Job Experience: </span>
+            </p>
+            <p>{jobs.experiences}</p>
+          </div>
         </div>
-    {/* job description */}
-    <div className="mb-4">  <p> <span className="font-bold">Job Description: </span>{jobs.job_description}</p></div>
-      {/* Educational requirements */}
-      <div className="mb-5"><p><span className="font-bold">Educational Requirements: </span></p>
-        <p>{jobs.educational_requirements}</p></div>
-        {/* job experience */}
-        <div className="mb-5"><p><span className="font-bold">Job Experience: </span></p>
-        <p>{jobs.experiences}</p></div>
-        
+        {/* job details card */}
+        <div className="px-4 pt-5 mx-auto lg:w-1/4 md:px-24 lg:px-8 bg-violet-100 mb-5 mt-5 h-150">
+          <p> Job details</p><hr className="border-1 border-black"/> 
+          
+          <p className="mt-3"> <span className="font-bold">Job Title: </span> {jobs.job_title}</p>
+          <p className="mt-3"><span className="font-bold">Salary: </span> {jobs.salary}</p>
+          <hr className="border-1 border-black"/>
+        </div>
       </div>
-      <div className="px-4 pt-5 mx-auto lg:w-1/4 md:px-24 lg:px-8">
-
-      </div>
-      </div>
-      
     </>
   );
 };
 
 export default Statistics;
- 
